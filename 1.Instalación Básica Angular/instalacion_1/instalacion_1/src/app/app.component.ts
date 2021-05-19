b98d7c7b198afd4a14e5963a3d1049cffb2ed516
duran221 <cristianfernandoduran@gmail.com>
2021-05-19 11:16:06 -0500

    Subida contenido del curso

---
+++ 1.Instalación Básica Angular/instalacion_1/instalacion_1/src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Hello hola {{name}}</h1>`,
+})
+export class AppComponent  { name = 'Angular 4 Mi primer proyecto OK'; }
